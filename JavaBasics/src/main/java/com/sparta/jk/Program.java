6cd3b6e60f56580a5cc7e4f92646ff52f8f8590a
jack-king1 <jackking.gm97@gmail.com>
2024-06-17 14:59:14 +0100

    Removed printing from greeting method, now returns string + reformatted

---
+++ JavaBasics/src/main/java/com/sparta/jk/Program.java
@@ -0,0 +1,27 @@
+package com.sparta.jk;
+
+public class Program {
+    public static void main(String[] args)
+    {
+        int timeOfDay = 21;
+        System.out.println(getTimeofDay(timeOfDay));
+    }
+
+    private static String getTimeofDay(int timeOfDay) {
+        String greeting = "";
+
+        if (timeOfDay >= 5 && timeOfDay <= 12)
+        {
+            greeting = "Good morning!";
+        }
+        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        {
+            greeting = "Good afternoon!";
+        }
+        else
+        {
+            greeting = "Good evening!";
+        }
+        return greeting;
+    }
+}

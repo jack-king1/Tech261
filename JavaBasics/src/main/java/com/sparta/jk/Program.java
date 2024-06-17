8a723d9c32b4ba5a87bba764fe265e4c7d65b495
jack-king1 <jackking.gm97@gmail.com>
2024-06-17 16:57:41 +0100

    Refactored code and removed magic numbers into new static helper class

--- JavaBasics/src/main/java/com/sparta/jk/Program.java
+++ JavaBasics/src/main/java/com/sparta/jk/Program.java
@@ -3,25 +3,25 @@ package com.sparta.jk;
 public class Program {
     public static void main(String[] args)
     {
-        int timeOfDay = 21;
+        int timeOfDay = 5;
         System.out.println(getTimeofDay(timeOfDay));
     }
 
-    public static String getTimeofDay(int timeOfDay) {
-        String greeting = "";
+    public static class TimeofDay{
+        public static int morningStart = 5;
+        public static int afterNoonStart = 12;
+        public static int eveningStart = 18;
+        public static int endOfDay = 23;
+        public static String[] greetings = {"Good Morning!", "Good Afternoon!", "Good Evening!"};
+    }
 
-        if (timeOfDay >= 5 && timeOfDay <= 12)
-        {
-            greeting = "Good morning!";
-        }
-        else if (timeOfDay >= 12 && timeOfDay <= 18)
-        {
-            greeting = "Good afternoon!";
-        }
-        else
-        {
-            greeting = "Good evening!";
-        }
-        return greeting;
+    public static String getTimeofDay(int timeOfDay) {
+        int currentTime = Math.abs(timeOfDay);
+        if(currentTime > TimeofDay.endOfDay) return "Invalid Time";
+
+        if(currentTime >= TimeofDay.morningStart && currentTime < TimeofDay.afterNoonStart) return TimeofDay.greetings[0];
+        else if (currentTime > TimeofDay.morningStart && currentTime <= TimeofDay.eveningStart) return TimeofDay.greetings[1];
+        else return TimeofDay.greetings[2];
+
     }
 }
--- JavaBasics/src/test/java/com/sparta/jk/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/jk/ProgramTest.java
@@ -6,17 +6,51 @@ import org.junit.jupiter.api.Test;
 
 public class ProgramTest {
 
-    @Test //Carries meta data. Passes this to JVM
-    @DisplayName("This is my first test!") //This makes reading the console output more readable, not a replacement
-    public void basicTest(){
-        Assertions.assertTrue(5 == 5, "Failed the test!!!!");
+    @Test
+    @DisplayName("Given a time of 18, then the greeting should be evening!")
+    void checkThat3ivesGoodEvening(){
+        String exepctedGreeting = Program.TimeofDay.greetings[2];
+        String actualGteeting = Program.getTimeofDay(21);
+        Assertions.assertEquals(exepctedGreeting, actualGteeting);
     }
 
     @Test
-    @DisplayName("Given a time of 21, then the greeting should be evening!")
-    void checkThat21ivesGoodEvening(){
-        String exepctedGreeting = "Good evening!";
-        String actualGteeting = Program.getTimeofDay(21);
+    @DisplayName("Given a time of 12, then the greeting should be Good Afternoon!")
+    void checkThat13ivesGoodAfternoon(){
+        String exepctedGreeting = Program.TimeofDay.greetings[1];
+        String actualGteeting = Program.getTimeofDay(13);
+        Assertions.assertEquals(exepctedGreeting, actualGteeting);
+    }
+
+    @Test
+    @DisplayName("Given a time of 5, then the greeting should be Good Morning!")
+    void checkThat5ivesGoodMorning(){
+        String exepctedGreeting = Program.TimeofDay.greetings[0];
+        String actualGteeting = Program.getTimeofDay(5);
+        Assertions.assertEquals(exepctedGreeting, actualGteeting);
+    }
+
+    @Test
+    @DisplayName("Given a time of 24 the outcome should be Invalid Time")
+    void checkThatTimeisWithinBounds(){
+        String exepctedGreeting = "Invalid Time";
+        String actualGteeting = Program.getTimeofDay(24);
+        Assertions.assertEquals(exepctedGreeting, actualGteeting);
+    }
+
+    @Test
+    @DisplayName("Given a time of 0 the outcome should be Good Evening!")
+    void checkThatTimeisWithinBoundsWithZero(){
+        String exepctedGreeting = Program.TimeofDay.greetings[2];
+        String actualGteeting = Program.getTimeofDay(0);
+        Assertions.assertEquals(exepctedGreeting, actualGteeting);
+    }
+
+    @Test
+    @DisplayName("Given a time of -7 the outcome should be Good Morning!")
+    void checkThatNegativeTimesConvertCorrectly(){
+        String exepctedGreeting = Program.TimeofDay.greetings[0];
+        String actualGteeting = Program.getTimeofDay(-7);
         Assertions.assertEquals(exepctedGreeting, actualGteeting);
     }
 }

54ed6a23f8baca8a295d4b0b46cf997afe2f020f
jack-king1 <jackking.gm97@gmail.com>
2024-06-17 15:10:02 +0100

    Added junit and simple program unit test

---
+++ JavaBasics/src/test/java/com/sparta/jk/ProgramTest.java
@@ -0,0 +1,11 @@
+package com.sparta.jk;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void basicTest(){
+        Assertions.assertTrue(5 == 5, "Failed the test!!!!");
+    }
+}
